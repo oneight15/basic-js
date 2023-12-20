24c3a291c760dae22b67530d657094d9566324d0
oneight15 <oneight1508@gmail.com>
2023-12-20 19:49:27 +0500

    feat: add solution carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(+sampleActivity) || !isFinite(+sampleActivity) || +sampleActivity <= 0 || +sampleActivity >= 15) {
+    return false;
+  } else {
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    const t = Math.log(MODERN_ACTIVITY / +sampleActivity) / k;
+  
+    return Math.ceil(t);
+  }
 }
 
 module.exports = {
